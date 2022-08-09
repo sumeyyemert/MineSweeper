5cd052d9694558385a4d6aa25af3f0c89ab126d8
Sümeyye Mert <sumeyye.mert@marun.edu.tr>
2022-08-09 16:05:38 +0300

    MineSweeper

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,8 @@
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
         MineSweeper mine = new MineSweeper();
         mine.run();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /*
-        MineSweeper mine = new MineSweeper();
-        mine.start();
-        mine.run(); */
     }
 }
